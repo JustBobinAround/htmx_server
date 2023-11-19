6368d0501770c7e95bd4880ebaeb6e19f8228704
JustBobinAround <roberthenryjunkins@gmail.com>
2023-11-19 16:34:32 -0500

    Added a Global type to replace Arc<Mutex<T>>

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,31 @@ pub mod prelude {
     pub use std::sync::{ Arc, Mutex };
     pub use maud::html;
     pub use crate::{server, lock_globals};
+    pub struct Global<T> {
+        inner: Arc<Mutex<T>>,
+    }
+
+    impl<T> Global<T> {
+        pub fn new(initial_value: T) -> Self {
+            Global {
+                inner: Arc::new(Mutex::new(initial_value)),
+            }
+        }
+
+        pub fn clone(&self) -> Self {
+            Global {
+                inner: self.inner.clone(),
+            }
+        }
+
+        pub fn lock(&self) -> Result<std::sync::MutexGuard<'_, T>, std::sync::PoisonError<std::sync::MutexGuard<'_, T>>> {
+            self.inner.lock()
+        }
+
+        pub fn try_lock(&self) -> Result<std::sync::MutexGuard<'_, T>, std::sync::TryLockError<std::sync::MutexGuard<'_, T>>>{
+            self.inner.try_lock()
+        }
+    }
 }
 
 pub mod macros;
