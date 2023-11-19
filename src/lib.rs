be1c201e6f52f7608f5533312ec5e92056ffe3d5
JustBobinAround <roberthenryjunkins@gmail.com>
2023-11-19 16:12:30 -0500

    fixed prelude deps

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 pub mod prelude {
     pub use async_std::prelude::*;
     pub use htmx_comp_macro::{global,htmx_comp};
-    //pub use server, lock_globals
     pub use lazy_static::lazy_static;
     pub use std::sync::{ Arc, Mutex };
     pub use maud::html;
